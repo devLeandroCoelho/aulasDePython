b1deae935a384a794f8f4c4e65451479f40e305d
devLeandroCoelho <devleandrocoelho@gmail.com>
2022-01-12 22:13:42 -0300

    Aula 04

---
+++ aula04/aula04.py
@@ -0,0 +1,12 @@
+"""
+Tipos de dados
+str - string - textos 'Assim' "Assim"
+int - inteiro - 12345 0 -10 -20 -40
+float - real/ponto flutuante - 10.50 1.5 -10.10 -50.93 0.0
+bool - booleano/logico - True/False 10==10
+"""
+
+print('Luiz', type('Luiz'))
+print(10, type(10))
+print(29.32, type(29.32))
+print('L' == 'L', type('L' == 'L'))
