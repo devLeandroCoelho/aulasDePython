cca617da24ca7bcadf7b8a4d9bf3814b515c1008
unknown <devleandrocoelho@gmail.com>
2022-01-12 22:02:35 -0300

    Primeiras 3 aulas de Python

---
+++ Aula01/aula01.py
@@ -0,0 +1,11 @@
+print('Linha 1')
+print('Linha 2') # comentário
+# Falando sobre do que se trata essa linha
+print('Linha 3')
+print('Linha 4')
+print('Linha 5')
+print('Linha 6')
+
+""" Comentario de varias linhas
+Podemos utilizar para pular linhas"""
+
---
+++ Aula02/aula02.py
@@ -0,0 +1,4 @@
+print('Luiz', 'Otavio', sep='-')
+print('Leandro', 'Coelho', sep=' ')
+print('047','505', '579', sep='.', end='-')
+print('94')
